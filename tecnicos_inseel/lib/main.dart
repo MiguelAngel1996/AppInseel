68eb2a44903595b85a7e5d8d2f264ff92898695a
MiguelAngel1996 <miguel14orozco@gmail.com>
2024-12-19 20:23:58 -0500

    Terminar provider y diseño inicial listView builder

--- tecnicos_inseel/lib/main.dart
+++ tecnicos_inseel/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:tecnicos_inseel/controllers/ots_provider.dart';
 import 'package:tecnicos_inseel/views/pages/home.dart';
+import 'package:provider/provider.dart';
 
 
 void main() {
@@ -12,25 +14,28 @@ class TecnicosInseel extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Tecnicos Inseel',
-      theme: ThemeData(
-        fontFamily: 'oxanium',
-        colorScheme: const ColorScheme(
-          brightness: Brightness.light,
-          primary: Color(0xff595959),
-          onPrimary: Color(0xfff2f2f2),
-          secondary: Color(0xfff2811d),
-          onSecondary: Color(0xfff2f2f2),
-          error: Colors.red,
-          onError: Colors.white,
-          surface: Color.fromARGB(255, 255, 197, 126),
-          onSurface: Color(0xff595959)
-          ),
-        useMaterial3: true,
+    return ChangeNotifierProvider(
+      create: (context) => OtsProvider(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Tecnicos Inseel',
+        theme: ThemeData(
+          fontFamily: 'oxanium',
+          colorScheme: const ColorScheme(
+            brightness: Brightness.light,
+            primary: Color(0xff595959),
+            onPrimary: Color(0xfff2f2f2),
+            secondary: Color(0xfff2811d),
+            onSecondary: Color(0xfff2f2f2),
+            error: Colors.red,
+            onError: Colors.white,
+            surface: Color.fromARGB(255, 255, 197, 126),
+            onSurface: Color(0xff595959)
+            ),
+          useMaterial3: true,
+        ),
+        home: const Home(),
       ),
-      home: const Home(),
     );
   }
 }
