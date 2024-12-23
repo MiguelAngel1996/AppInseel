53747d9e1396f4a44ed05c78df3aa1fbbb268d53
MiguelAngel1996 <miguel14orozco@gmail.com>
2024-12-23 13:56:36 -0500

    pantalla 'información inicial' con los TextField (Solo String)

--- tecnicos_inseel/lib/controllers/ots_provider.dart
+++ tecnicos_inseel/lib/controllers/ots_provider.dart
@@ -2,6 +2,124 @@ import 'package:flutter/material.dart';
 import 'package:tecnicos_inseel/models/ot.dart';
 
 class OtsProvider extends ChangeNotifier {
+    
+  Ot nuevaOt = Ot(
+    numeroOt: '',
+    fechaInicio: '',
+    horaInicio: '',
+    cliente: '',
+    sucursal: '',
+    ciudad: '',
+    motivo: '',
+    diagnostico: '',
+    trabajos: '',
+    comentarios: '',
+    tecnico1: '',
+    tecnico2: '',
+    fechaFin: '',
+    horaFin: '',
+    gastoReal: '',
+    recibe: '',
+    alturas: false,
+    estado: false,
+    materiales: {},
+  );
+
+  void actualizarNumeroOt(String nuevoNumeroOt) {
+    nuevaOt.numeroOt = nuevoNumeroOt;
+    notifyListeners();
+  }
+
+  void actualizarFechaInicio(String nuevoFechaInicio) {
+    nuevaOt.fechaInicio = nuevoFechaInicio;
+    notifyListeners();
+  }
+
+  void actualizarHoraInicio(String nuevoHoraInicio) {
+    nuevaOt.horaInicio = nuevoHoraInicio;
+    notifyListeners();
+  }
+
+  void actualizarCliente(String nuevoCliente) {
+    nuevaOt.cliente = nuevoCliente;
+    notifyListeners();
+  }
+
+  void actualizarSucursal(String nuevoSucursal) {
+    nuevaOt.sucursal = nuevoSucursal;
+    notifyListeners();
+  }
+
+  void actualizarCiudad(String nuevoCiudad) {
+    nuevaOt.ciudad = nuevoCiudad;
+    notifyListeners();
+  }
+
+  void actualizarMotivo(String nuevoMotivo) {
+    nuevaOt.motivo = nuevoMotivo;
+    notifyListeners();
+  }
+
+  void actualizarDiagnostico(String nuevoDiagnostico) {
+    nuevaOt.diagnostico = nuevoDiagnostico;
+    notifyListeners();
+  }
+
+  void actualizarTrabajos(String nuevoTrabajos) {
+    nuevaOt.trabajos = nuevoTrabajos;
+    notifyListeners();
+  }
+
+  void actualizarComentarios(String nuevoComentarios) {
+    nuevaOt.comentarios = nuevoComentarios;
+    notifyListeners();
+  }
+
+  void actualizarTecnico1(String nuevoTecnico1) {
+    nuevaOt.tecnico1 = nuevoTecnico1;
+    notifyListeners();
+  }
+
+  void actualizarTecnico2(String nuevoTecnico2) {
+    nuevaOt.tecnico2 = nuevoTecnico2;
+    notifyListeners();
+  }
+
+  void actualizarFechaFin(String nuevoFechaFin) {
+    nuevaOt.fechaFin = nuevoFechaFin;
+    notifyListeners();
+  }
+
+  void actualizarHoraFin(String nuevoHoraFin) {
+    nuevaOt.horaFin = nuevoHoraFin;
+    notifyListeners();
+  }
+
+  void actualizarGastoReal(String nuevoGastoReal) {
+    nuevaOt.gastoReal = nuevoGastoReal;
+    notifyListeners();
+  }
+
+  void actualizarRecibe(String nuevoRecibe) {
+    nuevaOt.recibe = nuevoRecibe;
+    notifyListeners();
+  }
+
+  void actualizarAlturas(bool nuevoAlturas) {
+    nuevaOt.alturas = nuevoAlturas;
+    notifyListeners();
+  }
+
+  void actualizarEstado(bool nuevoEstado) {
+    nuevaOt.estado = nuevoEstado;
+    notifyListeners();
+  }
+
+  void actualizarMateriales(Map<String,int> nuevoMateriales) {
+    nuevaOt.materiales = nuevoMateriales;
+    notifyListeners();
+  }
+
   final List<Ot> _listaOts = [
     Ot(
       numeroOt: '0001',
@@ -24,35 +142,15 @@ class OtsProvider extends ChangeNotifier {
       estado: true,
       materiales: {'Lamparas': 2, 'Tornillos': 4},
     ),
-    Ot(
-      numeroOt: '0002',
-      fechaInicio: '23/06/2024',
-      horaInicio: '10:00',
-      cliente: 'Terpel',
-      sucursal: 'Neira',
-      ciudad: 'Neira',
-      motivo: 'Daño desagüe',
-      diagnostico: 'sifón dañado',
-      trabajos: 'Cambio de rejilla',
-      comentarios: 'Se realizó el cambio de rejilla',
-      tecnico1: 'jeaky',
-      tecnico2: 'jeis',
-      fechaFin: '02/12/2024',
-      horaFin: '16:20',
-      gastoReal: '5 horas',
-      recibe: 'Felipe',
-      alturas: true,
-      estado: true,
-      materiales: {'Rejillas': 1, 'masilla': 1},
-    )
   ];
 
-  List<Ot> get listasOt{
+  List<Ot> get listasOt {
     return _listaOts;
   }
 
-  void addOT(Ot ot){
-    _listaOts.add(ot);
+
+  void addOT() {
+    _listaOts.add(nuevaOt);
     notifyListeners();
   }
 }
---
+++ tecnicos_inseel/lib/views/pages/informacion_inicial.dart
@@ -0,0 +1,353 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tecnicos_inseel/controllers/ots_provider.dart';
+
+class InformacionInicial extends StatefulWidget {
+  const InformacionInicial({super.key});
+
+  @override
+  State<InformacionInicial> createState() => _InformacionInicialState();
+}
+
+class _InformacionInicialState extends State<InformacionInicial> {
+  @override
+  Widget build(BuildContext context) {
+    final provider = Provider.of<OtsProvider>(context);
+    final nuevaOt = provider.nuevaOt;
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(80), // Altura personalizada
+        child: AppBar(
+          backgroundColor: Colors.transparent, // Fondo transparente
+          elevation: 0, // Sin sombra
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.onSecondary, // Fondo blanco
+              borderRadius: const BorderRadius.only(
+                bottomLeft: Radius.circular(20),
+                bottomRight: Radius.circular(20),
+              ),
+            ),
+            child: Center(
+              child: Text(
+                'Información Inicial',
+                style: TextStyle(
+                  fontSize: 24,
+                  color: Theme.of(context).colorScheme.primary,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+      backgroundColor: Theme.of(context).colorScheme.secondary,
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(30),
+              child: SizedBox(
+                width: double.infinity,
+                child: Column(                      
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'O.T. #:',
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodySmall!
+                          .copyWith(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Theme.of(context)
+                                  .colorScheme
+                                  .onSecondary),
+                    ),
+                    const SizedBox(height: 8),
+                    TextField(
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor:
+                            Theme.of(context).colorScheme.onSecondary,
+                        contentPadding: const EdgeInsets.symmetric(
+                          vertical: 16.0,
+                          horizontal: 20.0,
+                        ), // Padding interno del TextField
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(
+                              30), // Bordes redondeados
+                          borderSide: BorderSide.none, // Sin borde
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: BorderSide(
+                            color: Theme.of(context)
+                                .colorScheme
+                                .onSecondary, // Borde al enfocarse
+                            width: 2.0,
+                          ),
+                        ),
+                      ),
+                      onChanged: (value) {
+                        nuevaOt.numeroOt = value;
+                      },
+                    ),
+                    const SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Fecha inicio:',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodySmall!
+                                  .copyWith(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20,
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .onSecondary),
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            SizedBox(
+                              width: 200,
+                              child: TextField(
+                                decoration: InputDecoration(
+                                  filled: true,
+                                  fillColor: Theme.of(context)
+                                      .colorScheme
+                                      .onSecondary,
+                                  contentPadding:
+                                      const EdgeInsets.symmetric(
+                                    vertical: 16.0,
+                                    horizontal: 20.0,
+                                  ), // Padding interno del TextField
+                                  enabledBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(
+                                        30), // Bordes redondeados
+                                    borderSide:
+                                        BorderSide.none, // Sin borde
+                                  ),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(30),
+                                    borderSide: BorderSide(
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .onSecondary, // Borde al enfocarse
+                                      width: 2.0,
+                                    ),
+                                  ),
+                                ),
+                                onChanged: (value) {
+                                  nuevaOt.fechaInicio = value;
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Hora inicio:',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodySmall!
+                                  .copyWith(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20,
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .onSecondary),
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            SizedBox(
+                              width: 200,
+                              child: TextField(
+                                decoration: InputDecoration(
+                                  filled: true,
+                                  fillColor: Theme.of(context)
+                                      .colorScheme
+                                      .onSecondary,
+                                  contentPadding:
+                                      const EdgeInsets.symmetric(
+                                    vertical: 16.0,
+                                    horizontal: 20.0,
+                                  ), // Padding interno del TextField
+                                  enabledBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(
+                                        30), // Bordes redondeados
+                                    borderSide:
+                                        BorderSide.none, // Sin borde
+                                  ),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(30),
+                                    borderSide: BorderSide(
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .onSecondary, // Borde al enfocarse
+                                      width: 2.0,
+                                    ),
+                                  ),
+                                ),
+                                onChanged: (value) {
+                                  nuevaOt.horaInicio = value;
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      'Cliente:',
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodySmall!
+                          .copyWith(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Theme.of(context)
+                                  .colorScheme
+                                  .onSecondary),
+                    ),
+                    const SizedBox(height: 8),
+                    TextField(
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor:
+                            Theme.of(context).colorScheme.onSecondary,
+                        contentPadding: const EdgeInsets.symmetric(
+                          vertical: 16.0,
+                          horizontal: 20.0,
+                        ), // Padding interno del TextField
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(
+                              30), // Bordes redondeados
+                          borderSide: BorderSide.none, // Sin borde
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: BorderSide(
+                            color: Theme.of(context)
+                                .colorScheme
+                                .onSecondary, // Borde al enfocarse
+                            width: 2.0,
+                          ),
+                        ),
+                      ),
+                      onChanged: (value) {
+                        nuevaOt.cliente = value;
+                      },
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      'Sucursal:',
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodySmall!
+                          .copyWith(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Theme.of(context)
+                                  .colorScheme
+                                  .onSecondary),
+                    ),
+                    const SizedBox(height: 8),
+                    TextField(
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor:
+                            Theme.of(context).colorScheme.onSecondary,
+                        contentPadding: const EdgeInsets.symmetric(
+                          vertical: 16.0,
+                          horizontal: 20.0,
+                        ), // Padding interno del TextField
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(
+                              30), // Bordes redondeados
+                          borderSide: BorderSide.none, // Sin borde
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: BorderSide(
+                            color: Theme.of(context)
+                                .colorScheme
+                                .onSecondary, // Borde al enfocarse
+                            width: 2.0,
+                          ),
+                        ),
+                      ),
+                      onChanged: (value) {
+                        nuevaOt.sucursal = value;
+                      },
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      'Ciudad:',
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodySmall!
+                          .copyWith(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Theme.of(context)
+                                  .colorScheme
+                                  .onSecondary),
+                    ),
+                    const SizedBox(height: 8),
+                    TextField(
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor:
+                            Theme.of(context).colorScheme.onSecondary,
+                        contentPadding: const EdgeInsets.symmetric(
+                          vertical: 16.0,
+                          horizontal: 20.0,
+                        ), // Padding interno del TextField
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(
+                              30), // Bordes redondeados
+                          borderSide: BorderSide.none, // Sin borde
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: BorderSide(
+                            color: Theme.of(context)
+                                .colorScheme
+                                .onSecondary, // Borde al enfocarse
+                            width: 2.0,
+                          ),
+                        ),
+                      ),
+                      onChanged: (value) {
+                        nuevaOt.ciudad = value;
+                      },
+                    ),
+                    const SizedBox(height: 10),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Siguiente',
+        child: const Icon(Icons.arrow_forward_sharp),
+      ),
+    );
+  }
+}
