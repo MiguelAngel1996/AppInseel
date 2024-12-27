709c9d836f8ee1f22a3571042e820dca550db4a7
MiguelAngel1996 <miguel14orozco@gmail.com>
2024-12-27 18:46:59 -0500

    Llenado de formato ot completo

--- tecnicos_inseel/lib/views/components/vista_ot.dart
+++ tecnicos_inseel/lib/views/components/vista_ot.dart
@@ -12,6 +12,10 @@ class VistaOt extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<MapEntry<String, String>> pares = ot.materiales.entries.toList();
+    while (pares.length<10){
+      pares.add(const MapEntry('', ''));
+    }
     //final provider = Provider.of<OtsProvider>(context);
     //final listasOt = provider.listasOt;
     return Scaffold(
@@ -44,139 +48,244 @@ class VistaOt extends StatelessWidget {
       backgroundColor: Theme.of(context).colorScheme.surface,
       body: Padding(
         padding: const EdgeInsets.all(10),
-        child: Center(
-          child: LayoutBuilder(
-            builder: (context, constraints) {
-              return FutureBuilder<Size>(
-                future: _getImageSize('assets/images/OT_INSEEL.jpg'),
-                builder: (context, snapshot) {
-                  if (!snapshot.hasData) {
-                    return const CircularProgressIndicator(); // Mientras se cargan las dimensiones
-                  }
-
-                  final imageSize = snapshot.data!;
-                  final containerWidth = constraints.maxWidth;
-                  final containerHeight = constraints.maxHeight;
-
-                  // Calcular escala de la imagen
-                  final scaleWidth = containerWidth / imageSize.width;
-                  final scaleHeight = containerHeight / imageSize.height;
-                  final scale =
-                      scaleWidth < scaleHeight ? scaleWidth : scaleHeight;
-
-                  final scaledWidth = imageSize.width * scale;
-                  final scaledHeight = imageSize.height * scale;
-
-                  return SizedBox(
-                    width: scaledWidth,
-                    height: scaledHeight,
-                    child: Stack(
-                      children: [
-                        Image.asset(
-                          'assets/images/OT_INSEEL.jpg',
-                          fit: BoxFit.contain,
-                          width: scaledWidth,
-                          height: scaledHeight,
-                        ),
-                        VistaPrevia(
-                            scale: scale,
-                            top: 183,
-                            left: 890,
-                            dato: ot.numeroOt),
-                        VistaPrevia(
-                            scale: scale,
-                            top: 210,
-                            left: 890,
-                            dato: ot.fechaInicio),
-                        VistaPrevia(
-                            scale: scale,
-                            top: 237,
-                            left: 890,
-                            dato: ot.horaInicio),
-                        VistaPrevia(
-                            scale: scale,
-                            top: 265,
-                            left: 890,
-                            dato: ot.cliente),
-                        VistaPrevia(
-                            scale: scale,
-                            top: 293,
-                            left: 890,
-                            dato: ot.sucursal),
-                        VistaPrevia(
-                            scale: scale,
-                            top: 320,
-                            left: 890,
-                            dato: ot.ciudad),
-                        VistaPrevia(
-                            scale: scale,
-                            top: 380,
-                            left: 80,
-                            dato: ot.motivo),
-                        VistaPrevia(
-                            scale: scale,
-                            top: 460,
-                            left: 80,
-                            dato: ot.diagnostico),
-                        VistaPrevia(
-                            scale: scale,
-                            top: 542,
-                            left: 80,
-                            dato: ot.trabajos),
-                        VistaPrevia(
-                            scale: scale,
-                            top: 978,
-                            left: 80,
-                            dato: ot.comentarios),
-                        VistaPrevia(
-                            scale: scale,
-                            top: 1310,
-                            left: 800,
-                            dato: ot.tecnico1),
-                        VistaPrevia(
-                            scale: scale,
-                            top: 1350,
-                            left: 800,
-                            dato: ot.tecnico2),
-                        VistaPrevia(
-                            scale: scale,
-                            top: 1393,
-                            left: 800,
-                            dato: ot.fechaFin),
-                        VistaPrevia(
-                            scale: scale,
-                            top: 1429,
-                            left: 800,
-                            dato: ot.horaFin),
-                        VistaPrevia(
-                            scale: scale,
-                            top: 1468,
-                            left: 800,
-                            dato: ot.gastoReal),
-                        VistaPrevia(
-                            scale: scale,
-                            top: 1335,
-                            left: 230,
-                            dato: ot.recibe),
-                        VistaPrevia(
-                            scale: scale,
-                            top: 1220,
-                            left: 1000,
-                            dato: ot.alturas),
-                        VistaPrevia(
-                            scale: scale,
-                            top: 1220,
-                            left: 440,
-                            dato: ot.estado),
-                      ],
-                    ),
-                  );
-                },
-              );
-            },
+        child: SingleChildScrollView(
+          child: Center(
+            child: LayoutBuilder(
+              builder: (context, constraints) {
+                return FutureBuilder<Size>(
+                  future: _getImageSize('assets/images/OT_INSEEL.jpg'),
+                  builder: (context, snapshot) {
+                    if (!snapshot.hasData) {
+                      return const CircularProgressIndicator(); // Mientras se cargan las dimensiones
+                    }
+          
+                    final imageSize = snapshot.data!;
+                    final containerWidth = constraints.maxWidth;
+                    final containerHeight = constraints.maxHeight;
+          
+                    // Calcular escala de la imagen
+                    final scaleWidth = containerWidth / imageSize.width;
+                    final scaleHeight = containerHeight / imageSize.height;
+                    final scale =
+                        scaleWidth < scaleHeight ? scaleWidth : scaleHeight;
+          
+                    final scaledWidth = imageSize.width * scale;
+                    final scaledHeight = imageSize.height * scale;
+          
+                    return SizedBox(
+                      width: scaledWidth,
+                      height: scaledHeight,
+                      child: Stack(
+                        children: [
+                          Image.asset(
+                            'assets/images/OT_INSEEL.jpg',
+                            fit: BoxFit.fitWidth,
+                            width: scaledWidth,
+                            height: scaledHeight,
+                          ),
+                          VistaPrevia(
+                              scale: scale,
+                              top: 183,
+                              left: 890,
+                              dato: ot.numeroOt),
+                          VistaPrevia(
+                              scale: scale,
+                              top: 210,
+                              left: 890,
+                              dato: ot.fechaInicio),
+                          VistaPrevia(
+                              scale: scale,
+                              top: 237,
+                              left: 890,
+                              dato: ot.horaInicio),
+                          VistaPrevia(
+                              scale: scale,
+                              top: 265,
+                              left: 890,
+                              dato: ot.cliente),
+                          VistaPrevia(
+                              scale: scale,
+                              top: 293,
+                              left: 890,
+                              dato: ot.sucursal),
+                          VistaPrevia(
+                              scale: scale,
+                              top: 320,
+                              left: 890,
+                              dato: ot.ciudad),
+                          VistaPrevia(
+                              scale: scale,
+                              top: 380,
+                              left: 80,
+                              dato: ot.motivo),
+                          VistaPrevia(
+                              scale: scale,
+                              top: 460,
+                              left: 80,
+                              dato: ot.diagnostico),
+                          VistaPrevia(
+                              scale: scale,
+                              top: 542,
+                              left: 80,
+                              dato: ot.trabajos),
+                          VistaPrevia(
+                              scale: scale,
+                              top: 978,
+                              left: 80,
+                              dato: ot.comentarios),
+                          VistaPrevia(
+                              scale: scale,
+                              top: 1310,
+                              left: 800,
+                              dato: ot.tecnico1),
+                          VistaPrevia(
+                              scale: scale,
+                              top: 1350,
+                              left: 800,
+                              dato: ot.tecnico2),
+                          VistaPrevia(
+                              scale: scale,
+                              top: 1393,
+                              left: 800,
+                              dato: ot.fechaFin),
+                          VistaPrevia(
+                              scale: scale,
+                              top: 1429,
+                              left: 800,
+                              dato: ot.horaFin),
+                          VistaPrevia(
+                              scale: scale,
+                              top: 1468,
+                              left: 800,
+                              dato: ot.gastoReal),
+                          VistaPrevia(
+                              scale: scale,
+                              top: 1335,
+                              left: 230,
+                              dato: ot.recibe),
+                          VistaPrevia(
+                              scale: scale,
+                              top: 1220,
+                              left: 1000,
+                              dato: ot.alturas),
+                          VistaPrevia(
+                              scale: scale,
+                              top: 1220,
+                              left: 440,
+                              dato: ot.estado),
+                          VistaPrevia(
+                              scale: scale,
+                              top: 813,
+                              left: 90,
+                              dato: pares[0].value),
+                          VistaPrevia(
+                              scale: scale,
+                              top: 813,
+                              left: 210,
+                              dato: pares[0].key),
+                          VistaPrevia(
+                              scale: scale,
+                              top: 839,
+                              left: 90,
+                              dato: pares[1].value),
+                          VistaPrevia(
+                              scale: scale,
+                              top: 839,
+                              left: 210,
+                              dato: pares[1].key),
+                          VistaPrevia(
+                              scale: scale,
+                              top: 867,
+                              left: 90,
+                              dato: pares[2].value),
+                          VistaPrevia(
+                              scale: scale,
+                              top: 867,
+                              left: 210,
+                              dato: pares[2].key),
+                          VistaPrevia(
+                              scale: scale,
+                              top: 894,
+                              left: 90,
+                              dato: pares[3].value),
+                          VistaPrevia(
+                              scale: scale,
+                              top: 894,
+                              left: 210,
+                              dato: pares[3].key),
+                          VistaPrevia(
+                              scale: scale,
+                              top: 921,
+                              left: 90,
+                              dato: pares[4].value),
+                          VistaPrevia(
+                              scale: scale,
+                              top: 921,
+                              left: 210,
+                              dato: pares[4].key),
+                          VistaPrevia(
+                              scale: scale,
+                              top: 813,
+                              left: 640,
+                              dato: pares[5].value),
+                          VistaPrevia(
+                              scale: scale,
+                              top: 813,
+                              left: 780,
+                              dato: pares[5].key),
+                          VistaPrevia(
+                              scale: scale,
+                              top: 839,
+                              left: 640,
+                              dato: pares[6].value),
+                          VistaPrevia(
+                              scale: scale,
+                              top: 839,
+                              left: 780,
+                              dato: pares[6].key),
+                          VistaPrevia(
+                              scale: scale,
+                              top: 867,
+                              left: 640,
+                              dato: pares[7].value),
+                          VistaPrevia(
+                              scale: scale,
+                              top: 867,
+                              left: 780,
+                              dato: pares[7].key),
+                          VistaPrevia(
+                              scale: scale,
+                              top: 894,
+                              left: 640,
+                              dato: pares[8].value),
+                          VistaPrevia(
+                              scale: scale,
+                              top: 894,
+                              left: 780,
+                              dato: pares[8].key),
+                          VistaPrevia(
+                              scale: scale,
+                              top: 921,
+                              left: 640,
+                              dato: pares[9].value),
+                          VistaPrevia(
+                              scale: scale,
+                              top: 921,
+                              left: 780,
+                              dato: pares[9].key),
+                        ],
+                      ),
+                    );
+                  },
+                );
+              },
+            ),
           ),
         ),
       ),
+      floatingActionButton: FloatingActionButton(onPressed: (){},
+      tooltip: 'Compartir',
+        child: const Icon(Icons.share),),
     );
   }
 
